d59329f15e09c0124fc0720ed8f723aae5d90e4f
Yvan Ngalle <nycorpdev@gmail.com>
2024-09-09 13:19:16 +0100

    Update const response from config to class

--- src/Models/ResponseCode.php
+++ src/Models/ResponseCode.php
@@ -4,42 +4,35 @@ namespace Nycorp\LiteApi\Models;
 
 class ResponseCode
 {
-    public static function token() {
-        return new class {
-            public const EXPIRED = -1;
-            public const BLACK_LISTED = -2;
-            public const INVALID = -3;
-            public const NOT_FOUND = -4;
-            public const USER_NOT_FOUND = -5;
-        };
-    }
+    #Token
+    public const TOKEN_EXPIRED = -1;
+    public const TOKEN_BLACK_LISTED = -2;
+    public const TOKEN_INVALID = -3;
+    public const TOKEN_NOT_FOUND = -4;
+    public const TOKEN_USER_NOT_FOUND = -5;
 
-    public static function request() {
-        return new class {
-            public const SUCCESS = 1000;
-            public const FAILURE = -1001;
-            public const VALIDATION_ERROR = -1002;
-            public const EXPIRED = -1003;
-            public const TRYING_TO_INSERT_DUPLICATE = -1004;
-            public const NOT_AUTHORIZED = -1005;
-            public const EXCEPTION = -1006;
-            public const NOT_FOUND = -1007;
-            public const WRONG_JSON_FORMAT = -1008;
-            public const SERVICE_NOT_AVAILABLE = -1009;
-            public const EMERGENCY = -1010;
-        };
-    }
+    #Request
+    public const REQUEST_SUCCESS = 1000;
+    public const REQUEST_FAILURE = -1001;
+    public const REQUEST_VALIDATION_ERROR = -1002;
+    public const REQUEST_EXPIRED = -1003;
+    public const REQUEST_TRYING_TO_INSERT_DUPLICATE = -1004;
+    public const REQUEST_NOT_AUTHORIZED = -1005;
+    public const REQUEST_EXCEPTION = -1006;
+    public const REQUEST_NOT_FOUND = -1007;
+    public const REQUEST_WRONG_JSON_FORMAT = -1008;
+    public const REQUEST_SERVICE_NOT_AVAILABLE = -1009;
+    public const REQUEST_EMERGENCY = -1010;
 
-    public static function auth() {
-        return new class {
-            public const ACCOUNT_NOT_VERIFY = -1100;
-            public const WRONG_USERNAME = -1101;
-            public const WRONG_PASSWORD = -1102;
-            public const WRONG_CREDENTIALS = -1103;
-            public const ACCOUNT_VERIFIED = 1104;
-            public const NOT_EXISTS = -1105;
-        };
-    }
+    #Auth
+    public const AUTH_ACCOUNT_NOT_VERIFY = -1100;
+    public const AUTH_WRONG_USERNAME = -1101;
+    public const AUTH_WRONG_PASSWORD = -1102;
+    public const AUTH_WRONG_CREDENTIALS = -1103;
+    public const AUTH_ACCOUNT_VERIFIED = 1104;
+    public const AUTH_NOT_EXISTS = -1105;
+
+
 }
 
 
