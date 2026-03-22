340c962410cbe5735ce5e65a15be3939676cd5dc
Yvan Ngalle <nycorpdev@gmail.com>
2026-03-22 09:42:18 +0100

    refactor(Core): allow code comparison in response exception

--- src/Logging/ServiceLoggerHandler.php
+++ src/Logging/ServiceLoggerHandler.php
@@ -4,12 +4,12 @@ namespace Nycorp\LiteApi\Logging;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Level;
 use Monolog\LogRecord;
 use Nycorp\LiteApi\Http\Controllers\Core\LogLocalController;
 use Nycorp\LiteApi\Http\Service\LogRemoteService;
-use Ramsey\Uuid\Uuid;
 
 class ServiceLoggerHandler extends AbstractProcessingHandler
 {
@@ -18,7 +18,7 @@ class ServiceLoggerHandler extends AbstractProcessingHandler
     public function __construct(int|string|Level $level = Level::Debug, bool $bubble = true)
     {
         parent::__construct($level, $bubble);
-        $this->actionId = Uuid::uuid4()->toString();
+        $this->actionId = Str::uuid()->toString();
     }
 
     protected function write(LogRecord $record): void
